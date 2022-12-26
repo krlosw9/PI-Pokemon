a5e28fe17e03b5b796733e9f11dbb9cf4c9f1bf2
Carlos Waldo <krlosw9@gmail.com>
2022-12-25 19:27:06 -0500

    Cambio en CORS(express)de localhost:3000 a 192.168.0.102:3001 y en action de redux, para poder visualizar desde un telefono el llamado a la api

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,7 +2,7 @@ export const GET_ALL = 'GET_ALL_POKEMONS';
 
 export function getAll() {
   return (dispatch) => {
-    fetch('http://localhost:3001/pokemons')
+    fetch('http://192.168.0.102:3001/pokemons')
       .then( res => res.json())
       .then( data => dispatch({type: GET_ALL, payload: data}) )
   }
